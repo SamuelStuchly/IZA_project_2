43a24338b5789ab9e2c6856d65ede7ba3d663993
SamuelStuchly <sssstuchly@gmail.com>
2020-05-18 18:43:17 +0200

    package adjusted

--- sudoku/Package.swift
+++ sudoku/Package.swift
@@ -14,6 +14,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "sudoku",
+            dependencies: ["Parser"]),
+        .target(
+            name: "Parser",
+            dependencies: ["Board"]),
+        .target(
+            name: "Board",
+            dependencies: []),
+        .target(
+            name: "Solver",
             dependencies: []),
         .testTarget(
             name: "sudokuTests",
